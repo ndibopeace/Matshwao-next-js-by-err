8a241c88314612e5bebea941769aa05bdecb9570
Peace Mabaila <ndibopeace@gmail.com>
2026-08-26 16:13:31 +0200

    notes accesibility fixed

--- src/app/theory/layout.jsx
+++ src/app/theory/layout.jsx
@@ -3,16 +3,21 @@ import styles from './theoryLayout.module.css';
 
 // import Lesson1 from './personal-requirements/page';
 // for some reason i dont know, other lessons automatically import. 
-// All i need is to import this 1 lesson
+// All i need is to import this 1 lesson //
+
+export const metadata = {
+  title: "Theory lessons | err",
+  description: "Study driving theory lessons.",
+};
 
 export default function NotesLayout({children}) {
 
     return (
         <div className={styles.lesson_container}>
             <Aside />
-            <main>
+            <div>
                 {children}
-            </main>
+            </div>
         </div>
     )
 }
